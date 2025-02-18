38cb1ed2ec0fd1e9eb27fee0ad8b2c41c7e5eff1
yunus <yusun@smail.nju.edu.cn>
2025-02-19 01:01:02 +0800

    bug fix: cannot copy via ssh

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,22 @@ vim.g.maplocalleader = " "
 
 local opt = vim.opt
 opt.wrap = true
+
+-- Clipboard
+if vim.env.SSH_TTY then
+  opt.clipboard:append("unnamedplus")
+  local function paste()
+    return vim.split(vim.fn.getreg(""), "\n")
+  end
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = paste,
+      ["*"] = paste,
+    },
+  }
+end
